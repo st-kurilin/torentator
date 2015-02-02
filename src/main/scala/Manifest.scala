ff49ba2dfe69211599222d46f4131e9d366b3f58
stanislav-kurlin <sku@tradeshift.com>
2015-02-02 22:32:58 +0100

    Manifest parsing: multi file description.

--- src/main/scala/Manifest.scala
+++ src/main/scala/Manifest.scala
@@ -8,9 +8,12 @@ object Manifest {
                 val announce = m("announce") match {case BString(s) => s}
                 m("info") match {
                     case BDictionary(info) => 
-                        (info.get("piece"), info.get("length")) match {
-                            case (Some(BInteger(piece)), Some(BInteger(length))) => 
-                                new SingleFileManifest(new java.net.URL(announce), piece, length)        
+                        (info.get("piece"), info.get("length"), info.get("path")) match {
+                            case (Some(BInteger(piece)), Some(BInteger(length)), None) => 
+                                new SingleFileManifest(new java.net.URL(announce), piece, length)
+                            case (Some(BInteger(piece)), None, Some(BList(l))) => 
+                                val paths = l.map{case BString(s) => s}
+                                new MultiFileManifest(new java.net.URL(announce), piece, paths)
                         }
                         
                 }
@@ -23,7 +26,7 @@ sealed trait Manifest {
     def pieceLenght: Long
 }
 case class SingleFileManifest(announce: java.net.URL, pieceLenght: Long, length: Long) extends Manifest
-case class MultiFileManifest(announce: java.net.URL, pieceLenght: Long, paths: List[String]) extends Manifest {
+case class MultiFileManifest(announce: java.net.URL, pieceLenght: Long, paths: Seq[String]) extends Manifest {
     require(!paths.isEmpty)
 }    
 
--- src/test/scala/ManifestSpec.scala
+++ src/test/scala/ManifestSpec.scala
@@ -15,4 +15,15 @@ class ManifestSpec extends FlatSpec {
 
         assert(actual === new SingleFileManifest(new java.net.URL(announce), piece, lenght))
     }
+
+    it should "be creatable from multi file becoding description" in {
+        val announce = "http://tpb.tpb"
+        val piece = 36
+        val paths = List("foo/bar", "foo")
+
+        val actual = Manifest(BDictionary(Map("announce" -> BString(announce),
+            "info" -> BDictionary(Map("piece" -> BInteger(piece), "path" -> BList(paths.map(BString(_))))))))
+
+        assert(actual === new MultiFileManifest(new java.net.URL(announce), piece, paths))
+    }
 }
