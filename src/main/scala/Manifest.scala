6dddf562d1837b658880b8b8e8da56f096e3d42b
stanislav-kurlin <sku@tradeshift.com>
2015-02-09 22:08:01 +0100

    Reformat code.

--- src/main/scala/Manifest.scala
+++ src/main/scala/Manifest.scala
@@ -1,98 +1,98 @@
 package torentator
 
 object Manifest {
-    import Bencoding._
-    import util.Success
-    import util.Failure
-    import util.Try
-    def apply(location: java.io.File): util.Try[Manifest] = {
-        val content = java.nio.file.Files.readAllBytes(java.nio.file.Paths.get(location.getPath))
-        parse(new String(content, "ISO-8859-1")) flatMap { encoded => 
-            Manifest(encoded, infoHash(content))
-        }
+  import Bencoding._
+  import util.Success
+  import util.Failure
+  import util.Try
+  def apply(location: java.io.File): util.Try[Manifest] = {
+    val content = java.nio.file.Files.readAllBytes(java.nio.file.Paths.get(location.getPath))
+    parse(new String(content, "ISO-8859-1")) flatMap { encoded => 
+      Manifest(encoded, infoHash(content))
     }
+  }
 
-    def f(msg: String) = {
-        Failure(new RuntimeException(msg))
+  def f(msg: String) = {
+    Failure(new RuntimeException(msg))
+  }
+
+  def flatten[T](xs: Seq[Try[T]]): Try[Seq[T]] = {
+    val (ss: Seq[Success[T]]@unchecked, fs: Seq[Failure[T]]@unchecked) =
+    xs.partition(_.isSuccess)
+
+    if (fs.isEmpty) Success(ss map (_.get))
+    else Failure[Seq[T]](fs(0).exception) // Only keep the first failure
+  }
+
+  def apply(encoded: Bencode, hash: Seq[Byte]): util.Try[Manifest] = {
+    def decodeAnnounce(encoded: Bencode) = encoded match {
+      case BString(s) => Success(s)
+      case e => f(s"Manifest 'info' expected to be string, but [${e}] found.")   
     }
-
-    def flatten[T](xs: Seq[Try[T]]): Try[Seq[T]] = {
-      val (ss: Seq[Success[T]]@unchecked, fs: Seq[Failure[T]]@unchecked) =
-        xs.partition(_.isSuccess)
-
-      if (fs.isEmpty) Success(ss map (_.get))
-      else Failure[Seq[T]](fs(0).exception) // Only keep the first failure
+    def decodeInfo(announce: String, encoded: Bencode) = encoded match {
+      case BDictionary(info) =>
+        (info.get("name"), info.get("piece length"), info.get("length"), info.get("files")) match {
+          case (Some(BString(name)),
+            Some(BInteger(piece)),
+            Some(BInteger(length)),
+            None) => 
+            Success(new SingleFileManifest(name, new java.net.URI(announce), hash, piece, length))
+          case (Some(BString(name)), 
+            Some(BInteger(piece)),
+            None,
+            Some(BList(filesList))) => 
+            decodeFiles(filesList) map { case f =>
+              new MultiFileManifest(name, new java.net.URI(announce), hash, piece, f)
+            }
+          case (name, pieceLength, length, files) =>
+            f(s"""Can not determine structure of 'manifest.info':
+                name: ${name}, pieceLength: ${pieceLength},
+                length: ${length}, files: ${files}
+                available keys: ${info.keys}""")
+        }
+      case e => f(s"Manifest 'info' expected to be dictionary, but [${e}] found.")
     }
-
-    def apply(encoded: Bencode, hash: Seq[Byte]): util.Try[Manifest] = {
-        def decodeAnnounce(encoded: Bencode) = encoded match {
-            case BString(s) => Success(s)
-            case e => f(s"Manifest 'info' expected to be string, but [${e}] found.")   
+    def decodeFiles(encoded: Seq[Bencode]) = {
+      flatten(encoded map {
+        case BDictionary(m) => (m.get("length"), m.get("path")) match {
+          case (Some(BInteger(length)), Some(BList(path))) => 
+            flatten(path.map {
+              case BString(e) => Success(e)
+              case e => Failure(new RuntimeException("""'manifest.info.files.path' 
+                elements expected to be strings but [${e}] found"""))
+            }) map { l =>
+              l.foldLeft("") {
+                case (r, e) if r.isEmpty => e
+                case (r, e) => s"${r}/${e}"
+              }
+            } map { p => 
+              (length, p)
+            }
+          case e => f(s"""File in 'manifest.info.files' expected to be dictinary 
+            with 'length' and 'path' but [${e}] found.""")
         }
-        def decodeInfo(announce: String, encoded: Bencode) = encoded match {
-            case BDictionary(info) =>
-                (info.get("name"), info.get("piece length"), info.get("length"), info.get("files")) match {
-                    case (Some(BString(name)),
-                        Some(BInteger(piece)),
-                        Some(BInteger(length)),
-                        None) => 
-                        Success(new SingleFileManifest(name, new java.net.URI(announce), hash, piece, length))
-                    case (Some(BString(name)), 
-                        Some(BInteger(piece)),
-                        None,
-                        Some(BList(filesList))) => 
-                        decodeFiles(filesList) map { case f =>
-                            new MultiFileManifest(name, new java.net.URI(announce), hash, piece, f)
-                        }
-                    case (name, pieceLength, length, files) =>
-                        f(s"""Can not determine structure of 'manifest.info':
-                                name: ${name}, pieceLength: ${pieceLength},
-                                length: ${length}, files: ${files}
-                                available keys: ${info.keys}""")
-                }
-            case e => f(s"Manifest 'info' expected to be dictionary, but [${e}] found.")
-        }
-        def decodeFiles(encoded: Seq[Bencode]) = {
-            flatten(encoded map {
-                case BDictionary(m) => (m.get("length"), m.get("path")) match {
-                    case (Some(BInteger(length)), Some(BList(path))) => 
-                        flatten(path.map {
-                            case BString(e) => Success(e)
-                            case e => Failure(new RuntimeException("""'manifest.info.files.path' 
-                                elements expected to be strings but [${e}] found"""))
-                        }) map { l =>
-                            l.foldLeft("") {
-                                case (r, e) if r.isEmpty => e
-                                case (r, e) => s"${r}/${e}"
-                            }
-                        } map { p => 
-                            (length, p)
-                        }
-                    case e => f(s"""File in 'manifest.info.files' expected to be dictinary 
-                        with 'length' and 'path' but [${e}] found.""")
-                }
-                case e => f(s"""File in 'manifest.info.files'
-                    expected to be dictinary but ${e} found.""")
-            })
-        }
-        encoded match {
-            case BDictionary(m) =>
-                decodeAnnounce(m("announce")) flatMap {
-                    announce => decodeInfo(announce, m("info"))
-                }
-            case e => f(s"Manifest expected to be dictionary, but [${e}] found.")
-        }
+        case e => f(s"""File in 'manifest.info.files'
+          expected to be dictinary but ${e} found.""")
+      })
     }
+    encoded match {
+      case BDictionary(m) =>
+        decodeAnnounce(m("announce")) flatMap {
+          announce => decodeInfo(announce, m("info"))
+        }
+      case e => f(s"Manifest expected to be dictionary, but [${e}] found.")
+    }
+  }
 }
 
 sealed trait Manifest {
-    def name: String
-    def announce: java.net.URI
-    def pieceLenght: Long
-    def hash: Seq[Byte]
+  def name: String
+  def announce: java.net.URI
+  def pieceLenght: Long
+  def hash: Seq[Byte]
 }
 case class SingleFileManifest(name: String, announce: java.net.URI, 
-    hash: Seq[Byte], pieceLenght: Long, length: Long) extends Manifest
+  hash: Seq[Byte], pieceLenght: Long, length: Long) extends Manifest
 case class MultiFileManifest (name: String, announce: java.net.URI,
-    hash: Seq[Byte], pieceLenght: Long, files: Seq[(Long, String)]) extends Manifest
+  hash: Seq[Byte], pieceLenght: Long, files: Seq[(Long, String)]) extends Manifest
 
--- src/test/scala/ManifestSpec.scala
+++ src/test/scala/ManifestSpec.scala
@@ -3,78 +3,79 @@ package torentator
 import org.scalatest._
 
 class ManifestSpec extends FlatSpec with Matchers {
-    import Bencoding._
-    import util.{Success, Failure}
+  import Bencoding._
+  import util.{Success, Failure}
 
-    "Manifest" should "be creatable from single file becoding description" in {
-        val name = "tpb"
-        val announce = "http://tpb.tpb"
-        val piece = 36
-        val lenght = 200
-        val hash = List()
+  "Manifest" should "be creatable from single file becoding description" in {
+  val name = "tpb"
+  val announce = "http://tpb.tpb"
+  val piece = 36
+  val lenght = 200
+  val hash = List()
 
-        val actual = Manifest(BDictionary(Map(
-            "announce" -> BString(announce),
-            "info" -> BDictionary(Map(
-                "name"          -> BString(name),
-                "piece length"  -> BInteger(piece),
-                "length"        -> BInteger(lenght))))), hash)
+  val actual = Manifest(BDictionary(Map(
+    "announce" -> BString(announce),
+    "info" -> BDictionary(Map(
+    "name"    -> BString(name),
+    "piece length"  -> BInteger(piece),
+    "length"  -> BInteger(lenght))))), hash)
 
-        assert(actual === util.Success(
-            new SingleFileManifest(name, new java.net.URI(announce), hash, piece, lenght)))
+  assert(actual === util.Success(
+    new SingleFileManifest(name, new java.net.URI(announce), hash, piece, lenght)))
+  }
+
+  it should "be creatable from multi file becoding description" in {
+    val name = "tpb"
+    val announce = "http://tpb.tpb"
+    val piece = 36
+    val files = List((134L, "foo/bar"), (256L, "foo"))
+    val hash = List()
+
+    val filesEncoded = BList(files map {
+      case (l, f) => 
+      val path = BList(f.split("/").map(BString(_)))
+      BDictionary(Map("length" -> BInteger(l), "path" -> path))
+    })
+
+    val actual = Manifest(BDictionary(Map(
+      "announce" -> BString(announce),
+      "info" -> BDictionary(Map(
+      "name"    -> BString(name),
+      "piece length"  -> BInteger(piece),
+      "files"   -> filesEncoded)))), hash)
+
+    assert(actual === util.Success(
+    new MultiFileManifest(name, new java.net.URI(announce), hash, piece, files)))
+  }
+
+  it should "be creatable from real file (SingleFileManifest)" in {
+    val manifest = Manifest(new java.io.File("./src/test/resources/sample.single.http.torrent"))
+
+    manifest match {
+      case Success(SingleFileManifest(name, announce, hash, pieceLenght, length)) => 
+        name should not be empty
+        announce should not be (null)
+        hash should have size 20
+        pieceLenght should not be (0)
+        length should not be (0)
+        assert(hash === Bencoding.parseByteArray("1619ecc9373c3639f4ee3e261638f29b33a6cbd6"))
+      case Success(m) => fail(m.toString)
+      case Failure(e) => fail(e)
     }
+  }
 
-    it should "be creatable from multi file becoding description" in {
-        val name = "tpb"
-        val announce = "http://tpb.tpb"
-        val piece = 36
-        val files = List((134L, "foo/bar"), (256L, "foo"))
-        val hash = List()
+  it should "be creatable from real file (MultiFileManifest)" in {
+    val manifest = Manifest(new java.io.File("./src/test/resources/sample.multi.udp.torrent"))
 
-        val filesEncoded = BList(files map {
-                    case (l, f) => 
-                    val path = BList(f.split("/").map(BString(_)))
-                    BDictionary(Map("length" -> BInteger(l), "path" -> path))
-                })
-        val actual = Manifest(BDictionary(Map(
-            "announce" -> BString(announce),
-            "info" -> BDictionary(Map(
-                "name"          -> BString(name),
-                "piece length"  -> BInteger(piece),
-                "files"         -> filesEncoded)))), hash)
-
-        assert(actual === util.Success(
-            new MultiFileManifest(name, new java.net.URI(announce), hash, piece, files)))
+    manifest match {
+      case Success(MultiFileManifest(name, announce, hash, pieceLenght, files)) => 
+        name should not be empty
+        announce should not be (null)
+        hash should have size 20
+        pieceLenght should not be (0)
+        files should not be empty
+      case Success(m) => fail(m.toString)
+      case Failure(e) => fail(e)
     }
-
-    it should "be creatable from real file (SingleFileManifest)" in {
-        val manifest = Manifest(new java.io.File("./src/test/resources/sample.single.http.torrent"))
-
-        manifest match {
-            case Success(SingleFileManifest(name, announce, hash, pieceLenght, length)) => 
-                name should not be empty
-                announce should not be (null)
-                hash should have size 20
-                pieceLenght should not be (0)
-                length should not be (0)
-                assert(hash === Bencoding.parseByteArray("1619ecc9373c3639f4ee3e261638f29b33a6cbd6"))
-            case Success(m) => fail(m.toString)
-            case Failure(e) => fail(e)
-        }
-    }
-
-    it should "be creatable from real file (MultiFileManifest)" in {
-        val manifest = Manifest(new java.io.File("./src/test/resources/sample.multi.udp.torrent"))
-
-        manifest match {
-            case Success(MultiFileManifest(name, announce, hash, pieceLenght, files)) => 
-                name should not be empty
-                announce should not be (null)
-                hash should have size 20
-                pieceLenght should not be (0)
-                files should not be empty
-            case Success(m) => fail(m.toString)
-            case Failure(e) => fail(e)
-        }
-    }
+  }
 }
